2efa9acd7db255b37124204b1275296d52f5b5b8
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2019-04-30 16:26:16 -0400

    first commit

---
+++ shared-ride/src/main/java/com/redhat/samples/sharedride/Pickup.java
@@ -0,0 +1,120 @@
+package com.redhat.samples.sharedride;
+
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Map;
+
+import org.apache.kafka.common.errors.SerializationException;
+import org.apache.kafka.common.serialization.Deserializer;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class Pickup implements Serializer<Pickup>, Deserializer<Pickup>, Serde<Pickup> {
+
+	static Logger log = LoggerFactory.getLogger(Pickup.class);
+
+	static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+	Timestamp arrivaltime;
+	String speakerName;
+	String status;
+	Integer traveler;
+
+	@Override
+	public String toString() {
+		try {
+			return OBJECT_MAPPER.writeValueAsString(this);
+		} catch (Exception e) {
+			log.error("could not serialize", e);
+		}
+		return super.toString();
+	}
+
+	public static Pickup parse(String json) {
+		try {
+			return OBJECT_MAPPER.readValue(json, Pickup.class);
+		} catch (Exception e) {
+			log.error("could not parse", e);
+		}
+		return null;
+	}
+
+	public String getSpeakerName() {
+		return speakerName;
+	}
+
+	public void setSpeakerName(String speakerName) {
+		this.speakerName = speakerName;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public Integer getTraveler() {
+		return traveler;
+	}
+
+	public void setTraveler(Integer traveler) {
+		this.traveler = traveler;
+	}
+
+	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm:ss")
+	public Timestamp getArrivaltime() {
+		return arrivaltime;
+	}
+
+	public void setArrivaltime(Timestamp arrivaltime) {
+		this.arrivaltime = arrivaltime;
+	}
+
+	@Override
+	public Pickup deserialize(String topic, byte[] data) {
+		if (data == null) {
+			return null;
+		}
+		try {
+			return OBJECT_MAPPER.readValue(data, Pickup.class);
+		} catch (final IOException e) {
+			throw new SerializationException(e);
+		}
+	}
+
+	@Override
+	public void configure(Map configs, boolean isKey) {}
+
+	@Override
+	public byte[] serialize(String topic, Pickup data) {
+		if (data == null) {
+			return null;
+		}
+		try {
+			return OBJECT_MAPPER.writeValueAsBytes(data);
+		} catch (final Exception e) {
+			throw new SerializationException("Error serializing JSON message", e);
+		}
+	}
+
+	@Override
+	public void close() {}
+
+	@Override
+	public Serializer<Pickup> serializer() {
+		return this;
+	}
+
+	@Override
+	public Deserializer<Pickup> deserializer() {
+		return this;
+	}
+
+}
---
+++ shared-ride/src/main/java/com/redhat/samples/sharedride/SharedRideApplication.java
@@ -0,0 +1,94 @@
+package com.redhat.samples.sharedride;
+
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.Topology;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.kstream.Windowed;
+import org.apache.kafka.streams.processor.AbstractProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharedRideApplication implements CommandLineRunner {
+
+	private static final Logger log = LoggerFactory.getLogger(SharedRideApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharedRideApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Properties streamsConfiguration = new Properties();
+		streamsConfiguration.put(StreamsConfig.APPLICATION_ID_CONFIG, "transport-matcher");
+		streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG,
+				"my-cluster-kafka-bootstrap-streams.apps.summithack-d65b.openshiftworkshop.com:443");
+		streamsConfiguration.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
+		streamsConfiguration.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass().getName());
+		// streamsConfiguration.put(StreamsConfig.STATE_DIR_CONFIG, System.getProperty("java.io.tmpdir"));
+		streamsConfiguration.put(StreamsConfig.SECURITY_PROTOCOL_CONFIG, "SSL");
+		streamsConfiguration.put("ssl.keystore.location", "src/main/resources/keystore.jks");
+		streamsConfiguration.put("ssl.keystore.password", "password");
+		streamsConfiguration.put("ssl.truststore.location", "src/main/resources/keystore.jks");
+		streamsConfiguration.put("ssl.truststore.password", "password");
+
+		// Serializers/deserializers (serde) for String and Long types
+		final Serde<String> stringSerde = Serdes.String();
+		final Serde<Long> longSerde = Serdes.Long();
+		final Serde<Pickup> pickupSerde = new Pickup();
+
+		final StreamsBuilder builder = new StreamsBuilder();
+
+		// Construct a `KStream` from the input topic
+		KStream<String, String> pickups = builder.stream("pickups", Consumed.with(stringSerde, stringSerde));
+		KStream<Long, Pickup> pickupTimes = pickups
+			.map((key, value) -> {
+				Pickup pickup = Pickup.parse(value);
+				return KeyValue.pair(pickup.getArrivaltime().getTime(), pickup);
+			});
+		KTable<Windowed<Long>, Long> grouped = pickupTimes.groupByKey(Grouped.with(longSerde, pickupSerde))
+				.windowedBy(TimeWindows.of(Duration.ofMinutes(1)))
+				.aggregate(
+					() -> 0L, 
+					(aggKey, newValue, aggValue) -> aggValue + newValue.getTraveler());
+		
+		grouped.toStream().process(() -> new AbstractProcessor<Windowed<Long>, Long>() {
+				@Override
+				public void process(Windowed<Long> key, Long value) {
+					log.info("Pickup Time: " + SimpleDateFormat.getTimeInstance().format(new Timestamp(key.key())) + " Travelers: " + value);
+				}
+			});
+
+		final Topology topology = builder.build();
+
+		System.out.println(topology.describe());
+
+		KafkaStreams streams = new KafkaStreams(topology, streamsConfiguration);
+		
+		Runtime.getRuntime().addShutdownHook(new Thread() {
+            @Override
+            public void run() {
+				streams.close();
+            }
+		});
+		
+		streams.start();		
+	}
+}
