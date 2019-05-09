240de55335c9d02e1f567d691f5c6aa747637c5b
Hugo Guerrero <1001939+hguerrero@users.noreply.github.com>
2019-05-09 09:27:25 -0400

    new version

--- shared-ride/src/main/java/com/redhat/samples/sharedride/SharedRideApplication.java
+++ shared-ride/src/main/java/com/redhat/samples/sharedride/SharedRideApplication.java
@@ -15,6 +15,7 @@ import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.Joined;
+import org.apache.kafka.streams.kstream.KGroupedStream;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
 import org.apache.kafka.streams.kstream.Produced;
@@ -57,19 +58,50 @@ public class SharedRideApplication implements CommandLineRunner {
 
 		final StreamsBuilder builder = new StreamsBuilder();
 
-		// 1. Check how many passangers to pickup by time 
+		// 0. Check the below code example. Is getting the pickuptimes for your speakers. Take as a base for the next tasks.
+
+		// 1. Update the aggreggation function to sum the attendees instead of just adding the amount of records.
+
+		// 2. Copy the code and apply the same process to get the events records comming now from the userX-shareride topic
+		//    map the json values to a Vehicle helper class where the space available is the key and 
+		//    group the to find the cheapest car
+
+		// 3. Add a new transformation to the key from the pickupTimes table and map that to now get the pax pickup 
+		//    as the key and the pickup time as the value. Call this pickupSpace
+
+		// 4. Join the pickupSpace stream with the cheapest ride so you can match the prefered one.
+
+		// 6. Write the results back to AMQ Streams under the "userX-results" (use the .to() method) write it as a 
+		//    json string so you can take it back the google sheet. 
+
+		// 7. Enjoy!
+
+		/* ***************** */
+
+		// pickups KStream is receiving data for the "user1-pickups topic"
 		KStream<String, String> pickups = builder.stream("user1-pickups", Consumed.with(stringSerde, stringSerde));
-		KStream<Integer, String> pickupSpace = ...
-			
-		// 2. Check the cheapest ride by numer of available spaces
-		KStream<String, String> rides = builder.stream("user1-uber", Consumed.with(stringSerde, stringSerde));
-		KTable<Integer, Transport> cheapest = ...
-
-		// 3. Join the datasets to match time pickups with cheaper ride
-		KStream<Integer, String> matches = ...
-
-		// Send the results back to paste in the datasheet
-		matches.to("user1-results", Produced.with(integerSerde, stringSerde));
+		// raw data comes in the <String, String> format as it's a null Kafka key and a
+		// JSON String so we need to map the stream to transform the events into <Long, Pickup> 
+		KStream<Long, Pickup> mappedPickups = pickups.map((key, value) -> {
+			// Pickup pojo has a parse method to serialize and deserialize from json
+			Pickup pickup = Pickup.parse(value);
+			// for easy processing the new key is the milliseconds (Long) representing the pickup date
+		return KeyValue.pair(pickup.getArrivaltime().getTime(), pickup);
+		});
+		// with our data ready to process, we will group records by key (pickup time expresed in milliseconds)
+		// as we are changing the data type of the key and value we need to explicit set the ser/des types
+		KGroupedStream<Long, Pickup> groupedPickups = mappedPickups.groupByKey(Grouped.with(longSerde, pickupSerde));
+		// with the grouped records we can then execute an aggregation transformation and get a table 
+		// with the aggregated results
+		KTable<Long, Integer> pickupTimes = groupedPickups.aggregate(
+			// we need a counter initialized in 0
+			() -> 0,
+			// with each record we increment the counter
+			(aggKey, newValue, aggValue) -> aggValue + 1
+		);
+		// if we transform the table again to a stream, we can `peek` into the new stream to check the new records
+		pickupTimes.toStream().peek( 
+			(key, value) -> log.info("Pickup Time: " + value + " Travelers: " + key));
 
 		final Topology topology = builder.build();
 
